8a0ebe96833465599a7d050848009b54f17f47f9
wandsen <dsenwan@gmail.com>
2018-10-29 11:07:32 +0800

    adding classes

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,11 +1,31 @@
 class VideosController < ApplicationController
 
   def index
+    @playlist = Playlist.find(params[:playlist_id])
+    @video = Video.all
+
+  end
+
+  def new
     render plain:"test"
+
+  end
+
+  def create
+    @video = Video.new(video_params)
+    @video.save
+    render plain: "saved"
   end
 
   def show
-    render plain:"testshow"
+    byebug
+    @video = Video.find
+  end
+
+  private
+
+  def video_params
+    params.require(:video).permit(:video_url, :playlist_id)
   end
 
 end
