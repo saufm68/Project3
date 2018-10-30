610e50ac8a2ca5cb5ad5d8e6c5b2c3433f5830b2
ChloeLiang <nliangxin@gmail.com>
2018-10-30 11:07:24 +0800

    add uploads table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_30_014035) do
+ActiveRecord::Schema.define(version: 2018_10_30_025521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,11 @@ ActiveRecord::Schema.define(version: 2018_10_30_014035) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "uploads", force: :cascade do |t|
+    t.string "name"
+    t.string "url"
   end
 
   create_table "users", force: :cascade do |t|
